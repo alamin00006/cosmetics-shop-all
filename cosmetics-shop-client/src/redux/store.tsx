472f1d334f02eab75b93255d5fd7ee2a414de215
Mohammad Al Amin <alaminbamna08@gmail.com>
2025-06-29 18:46:13 +0600

    axios intance setup

--- cosmetics-shop-client/src/redux/store.tsx
+++ cosmetics-shop-client/src/redux/store.tsx
@@ -1,10 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { cartReducer } from "./reducers/cartSlice";
+import { baseApi } from "./api/baseApi";
 
 export const store = configureStore({
   reducer: {
     cart: cartReducer,
+    // pagination: paginationSlice,
+    [baseApi.reducerPath]: baseApi.reducer,
   },
+  // middleware: (getDefaultMiddleware) =>
+  //   getDefaultMiddleware().concat(baseApi.middleware),
 });
 
 // Infer the `RootState` and `AppDispatch` types from the store
